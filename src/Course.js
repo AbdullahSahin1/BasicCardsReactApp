459efe0da997002b4035f315caaabd08ae163d1a
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-02 19:50:19 +0300

    Update Course.js

--- src/Course.js
+++ src/Course.js
@@ -3,7 +3,22 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 function Course({image,title,description}) {
     return ( 
-       
+         <Card >
+        <CardMedia
+          sx={{ height: 140 }}
+          image={image} 
+          title="FrameWorks"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {description}
+          </Typography>
+        </CardContent>
+      
+      </Card>
      );
 }
 
