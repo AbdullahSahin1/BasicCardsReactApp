a07fd20ada4f58fd143f82a77ab7681eb440a921
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-02 19:33:44 +0300

    Update Course.js

--- src/Course.js
+++ src/Course.js
@@ -1,7 +1,9 @@
 function Course({image,title,description}) {
     return ( 
         <div>
-            
+              <img src={image}/>
+            <h3>{title}</h3>
+            <p>{description}</p>
         </div>
      );
 }
