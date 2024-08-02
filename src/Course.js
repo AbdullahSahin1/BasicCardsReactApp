18ae052b905dc545dda6e98dca4744017c1d3576
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-02 19:49:50 +0300

    Update Course.js

--- src/Course.js
+++ src/Course.js
@@ -1,3 +1,6 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 function Course({image,title,description}) {
     return ( 
         <div>
