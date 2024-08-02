29a334c940c899fb45bf10a9f55602d9a9b4e611
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-02 12:38:29 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+function App() {
+  return (
+    <div >
+        <AppBar>
+          <Toolbar sx={{fontSize:30, justifyContent:'center'}}>Kart Projesi</Toolbar>
+        </AppBar>
+    </div>
+  );
+}
+
+export default App;
